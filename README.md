ae558fea524df9b3da97a529556221e70c34eca9
autodotua <autodotua@outlook.com>
2020-03-27 10:49:02 +0800

    基本完成显示标签功能

--- README.md
+++ README.md
@@ -3,34 +3,58 @@
 ## 开发相关
 
 ### Shp转GeoJson
+>2020年3月23日
 
 很可能是因为NTS的BUG，在读取shapefile的dbf文件时，指定编码无效（包括构造函数和设置默认编码的静态属性）。需要在*.shp同级
 
 
 ### leaflet相关
-
+>2020年3月23日
 - leaflet图层没有Visible选项，因此当设置图层不可见时，直接移除图层
 
 ## 使用相关
 ### 关于GeoJSON
-
+>2020年3月23日
 - 性能较差，超过2MB的文件会有可感知操作延迟
-- 文件较大，大约是相同Shapefile的10倍大小
+- 文件较大，大约是相同Shapefile的2倍大小
 - 仅支持WGS84坐标系
 
 
 
 ## 待解决的问题
+
 ### GeoJson转换问题
-- 问题：NTS的GeoJson转换器或Shapefile的Reader有BUG，部分shapefile无法完整转换，部分Shapefile转换后无法读取。
-- 解决方案：
-  1. 使用C#重写Shapefile转GeoJson的方法
+>2020年3月24日
+
+问题：NTS的GeoJson转换器或Shapefile的Reader有BUG，部分shapefile无法完整转换，部分Shapefile转换后无法读取。
+解决方案：
+  - 使用C#重写Shapefile转GeoJson的方法
 
 ### Leaflet支持Geopackage问题
-- 问题：Leaflet-Geopackage的包无法安装
-- 解决方案：
+>2020年3月24日
+
+问题：Leaflet-Geopackage的包无法安装
+
+解决方案：
   - 放弃Geopackage的支持
   - 使用C#重写Geopackage转mbtiles的方法
   - 使用QGIS进行导出/转换
 
 
+## 日志
+
+### 3-21
+
+开始项目
+
+### 3-22
+
+完成对GeoJson和MtTiles格式的读写
+
+### 3-23
+
+基本完好对唯一值渲染器的读取和显示
+
+### 3-28
+
+基本完成显示标签功能
--- src/Map/MapView.vue
+++ src/Map/MapView.vue
@@ -12,7 +12,7 @@ export default {
   name: "MapView",
   components: {},
   data() {
-    return { currentFeature: "" };
+    return { currentFeature: "", sheet: {} };
   },
   props: ["feature-clicked", "geoJsons", "mbtiles"],
   computed: {},
@@ -21,7 +21,7 @@ export default {
       map = L.map("map", {
         zoomSnap: 0,
         zoomDelta: 0.5
-      }).setView([28, 119.5], 12);
+      }).setView([28, 119.53], 12);
       // }).setView([30.257, 119.727], 16);
       L.tileLayer("https://mt1.google.cn/vt/lyrs=s&x={x}&y={y}&z={z}", {
         maxZoom: 18,
@@ -55,11 +55,12 @@ export default {
       path = "map/style/" + name + ".style.json";
       response = await fetch(path);
       const styleJson = await response.json();
-console.log( name);
+      console.log(name);
 
       let layer = L.geoJSON(data, {
         style: feature => this.getStyle(styleJson, feature),
         onEachFeature: (feature, layer) => {
+          this.bindLabel(styleJson, layer, feature, name);
           layer.on("click", () => {
             this.$emit("feature-clicked", feature);
           });
@@ -73,10 +74,63 @@ console.log( name);
           layer.addTo(map);
         }
       };
+
       let geoJson = { obj: layer, visible: true, name, setVisiable };
       this.geoJsons.push(geoJson);
 
       return geoJson;
+    },
+    bindLabel(styleJson, layer, feature, layerName) {
+      if (this.sheet[layerName] == undefined) {
+        let sheet = this.createStyleSheet(layerName);
+        let color = this.getColor(styleJson.label.symbol.color);
+        let fontSize = styleJson.label.symbol.font.size * 1.5;
+        let borderSize = styleJson.label.symbol.haloSize * 1.5;
+        let borderColor = this.getColor(styleJson.label.symbol.haloColor);
+        sheet.insertRule(
+          `.label-${layerName}{
+        background: none;
+        border:none;
+        box-shadow: none;
+        }`,
+          0
+        );
+        sheet.insertRule(
+          `.label-${layerName}::before{
+        display:none;
+        }`,
+          0
+        );
+        sheet.insertRule(
+          `.label-${layerName} a{
+        font-size:${fontSize}px;
+        color:${color.color};
+         text-shadow:  0 0 ${borderSize}px ${borderColor.color};
+        }`,
+          0
+        );
+      }
+      //先在最后加一个空格，然后到时候替换的时候可以用来识别字段的结束
+      let content = styleJson.label.labelExpressionInfo.expression + " ";
+      for (const key in feature.properties) {
+        content = content.replace(
+          "$feature." + key + " ",
+          feature.properties[key]
+        );
+      }
+      layer.bindTooltip("<a>" + content + "</a>", {
+        permanent: true,
+        className: "label-" + layerName,
+        offset: [0, 0],
+        direction: "center"
+      });
+    },
+    createStyleSheet(layerName) {
+      var style = document.createElement("style");
+      style.type = "text/css";
+      document.head.appendChild(style);
+      this.sheet[layerName] = style.sheet;
+      return style.sheet;
     },
     // eslint-disable-next-line no-unused-vars
     getStyle(styleJson, feature) {
@@ -128,10 +182,8 @@ console.log( name);
         let path = "map/mbtiles/" + name + ".mbtiles";
         let mb = L.tileLayer.mbTiles(path, {}).addTo(map);
         mb.on("databaseerror", function(ev) {
-          let setVisible=()=>{
-
-          };
-          this.mbtiles.push({name,obj:mb,setVisible});
+          let setVisible = () => {};
+          this.mbtiles.push({ name, obj: mb, setVisible });
           console.info("MBTiles DB 错误：" + path, ev);
         });
       }
